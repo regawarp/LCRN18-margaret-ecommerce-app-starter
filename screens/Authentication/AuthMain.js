4d1bc17d3870fcb6a2c6999dbde3b73e80df7e27
regawarp <regawa04@gmail.com>
2022-08-16 09:22:50 +0700

    Revert "feature: setup anination for signin and signup form"
    This reverts commit d5d43316b2de49920d5c938bbfb85faf4bde9de7.

--- screens/Authentication/AuthMain.js
+++ screens/Authentication/AuthMain.js
@@ -1,111 +1,158 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    Image,
+    Modal,
+    Flatlist,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    StyleSheet
 } from 'react-native';
 
+import { MotiView, useAnimationState } from 'moti'
+import { Shadow } from 'react-native-shadow-2'
+
 import {
-    CountryDropDown
-} from "../../components";
+    TextButton
+} from '../../components';
+import {
+    icons,
+    images,
+    COLORS,
+    FONTS,
+    SIZES
+} from '../../constants';
 
 const AuthMain = () => {
 
-    // Country
-    const [countries, setCountries] = React.useState([])
-    const [showCountryModal, setShowCountryModal] = React.useState(false)
+    // States
+    const [mode, setMode] = React.useState("signIn");
+
+    // Animation States
+
+    const animationState = useAnimationState({
+        signIn: {
+            height: SIZES.height * 0.55
+        },
+        signUp: {
+            height: SIZES.height * 0.7
+        }
+    })
 
     React.useEffect(() => {
-        // Fetch countires
-        fetch("https://restcountries.com/v2/all")
-            .then(response => response.json())
-            .then(data => {
-                let countryData = data.map(item => {
-                    return {
-                        code: item.alpha2Code,
-                        name: item.name,
-                        callingCode: `+${item.callingCodes[0]}`,
-                        flag: `https://countryflagsapi.com/png/${item.alpha2Code}`
-                    }
-                })
-
-                setCountries(countryData)
-            })
+        animationState.transitionTo("signIn")
     }, [])
 
     // Render
 
-    function renderCountryModal() {
+    function renderSignIn() {
         return (
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={showCountryModal}
+            <MotiView
+                state={animationState}
+                style={{
+                    marginTop: SIZES.padding,
+                    height: SIZES.height * 0.55
+                }}
             >
-                <TouchableWithoutFeedback
-                    onPress={() => setShowCountryModal(false)}
-                >
+                <Shadow>
                     <View
-                        style={{
-                            flex: 1,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundColor: COLORS.dark80
-                        }}
+                        style={styles.authContainer}
                     >
-                        <View
+                        <Text
                             style={{
-                                height: 400,
-                                width: SIZES.width * 0.8,
-                                backgroundColor: COLORS.light,
-                                borderRadius: SIZES.radius
+                                width: '60%',
+                                lineHeight: 45,
+                                color: COLORS.dark,
+                                ...FONTS.h5
                             }}
                         >
-                            <FlatList
-                                data={countries}
-                                keyExtractor={(item) => item.code}
-                                contentContainerStyle={{
-                                    paddingHorizontal: SIZES.padding,
-                                    paddingBottom: SIZES.padding,
-                                }}
-                                renderItem={({ item }) => {
-                                    return (
-                                        <TouchableOpacity
-                                            style={{
-                                                flexDirection: 'row',
-                                                alignItems: 'center',
-                                                marginTop: SIZES.radius
-                                            }}
-                                            onPress={() => {
-                                                console.log(item)
-                                                setSelectedCountry(item)
-                                                setShowCountryModal(false)
-                                            }}
-                                        >
-                                            <Image
-                                                source={{ uri: item.flag }}
-                                                resizeMode="contain"
-                                                style={{
-                                                    width: 40,
-                                                    height: 30
-                                                }}
-                                            />
-                                            <Text style={{ flex: 1, marginLeft: SIZES.radius, ...FONTS.body3 }}>{item.name}</Text>
-                                        </TouchableOpacity>
-                                    )
-                                }}
-                            />
-                        </View>
+                            Sign in to continue
+                        </Text>
                     </View>
-                </TouchableWithoutFeedback>
-            </Modal>
+                </Shadow>
+
+            </MotiView>
         )
     }
 
-    return (
-        <View>
+    function renderSignUp() {
+        return (
+            <MotiView
+                state={animationState}
+                style={{
+                    marginTop: SIZES.padding,
+                }}
+            >
+                <Shadow>
+                    <View
+                        style={styles.authContainer}
+                    >
 
+                    </View>
+                </Shadow>
+
+            </MotiView>
+        )
+    }
+
+    function renderAuthContainer() {
+        if (mode == "signIn") {
+            return renderSignIn();
+        } else {
+            return renderSignUp();
+        }
+    }
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                paddingHorizontal: SIZES.padding,
+                backgroundColor: COLORS.lightGrey
+            }}
+        >
+            {/* Logo */}
+            <Image
+                source={images.logo}
+                style={{
+                    alignSelf: 'center',
+                    marginTop: SIZES.padding * 2,
+                    width: 50,
+                    height: 50
+                }}
+            />
+
+            {/* Auth Container */}
+            <View>
+                {renderAuthContainer()}
+            </View>
+
+            <TextButton
+                label="Toggle"
+                onPress={() => {
+                    if (animationState.current === "signIn") {
+                        animationState.transitionTo("signUp");
+                        setMode("signUp");
+                    } else {
+                        animationState.transitionTo("signIn");
+                        setMode("signIn");
+                    }
+                }}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    authContainer: {
+        flex: 1,
+        width: SIZES.width - (SIZES.padding * 2),
+        paddingHorizontal: SIZES.padding,
+        paddingVertical: SIZES.radius,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.light
+    }
+})
 
 export default AuthMain;
